a0814ca0bd7cf125e47efde9bbaee5c320e6d492
rgoldsmith33703 <rgolds33703@outlook.com>
2022-08-19 03:05:47 -0400

    works with operating on two numbers

--- script.js
+++ script.js
@@ -30,3 +30,34 @@ function operate(operator, num1, num2) {
             break
     }
 }
+
+let display = document.querySelector('#display');
+let digitButtons = document.querySelectorAll('.digit');
+let operatorButtons = document.querySelectorAll('.operator');
+let buttons = document.querySelectorAll('button');
+let equals = document.querySelector('#equals').textContent;
+
+let digits = [];
+let operators = [];
+digitButtons.forEach(button => digits.push(button.textContent));
+operatorButtons.forEach(button => operators.push(button.textContent));
+
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        if ((digits.includes(e.target.textContent))) {
+            display.textContent += e.target.textContent
+        } else if (operators.includes(e.target.textContent)) {
+            num1 = parseInt(display.textContent)
+            operator = e.target.textContent
+            display.textContent = ''
+        }  else if (e.target.textContent === equals) {
+            num2 = parseInt(display.textContent)
+            let result = operate(operator, num1, num2)
+            display.textContent = result
+        }
+    })
+})
+
+
+
+
